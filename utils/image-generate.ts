0935c87cfab450e565310ea82e4fdeb7a1f3f6a8
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-07-30 10:38:02 +0100

    FIX: not found error

--- utils/image-generate.ts
+++
@@ -1,9 +0,0 @@
-import ALL from "../ALL.config";
-
-const API_URL = ALL.RandomImagesEndPoint;
-let GenerateImage = async () => {
-  let images = await fetch(API_URL).then((response) => response.json());
-  // .then((result) => result.results);
-  return images[0];
-};
-export default GenerateImage;
